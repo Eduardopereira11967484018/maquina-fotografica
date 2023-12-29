4468dc6aeeee6f23df6e27e6dab101812af41647
portifoio0-1 <https://github.com/Eduardopereira11967484018/portifolio0-1.git>
2023-12-29 10:18:59 -0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+//Components
+import Hero from '../components/Hero.js';
+import LatestProducts from '../components/LatestProducts.js';
+
+const Home = () => {
+  return (
+    <section>
+      <Hero/>
+      <LatestProducts/>
+    </section>
+  )
+  
+};
+
+export default Home;
